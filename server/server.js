fe12e21a715a5e0fba1d3b8b26f17c989fb688cf
hrbcksq <hrbcksq@gmail.com>
2016-01-03 01:38:21 +0200

    unused lines

--- server/server.js
+++ server/server.js
@@ -1,11 +1,3 @@
-// global.app_require = function(name) {
-// 	console.log(__dirname + '/' + name);
-//     return require(__dirname + '/' + name);
-// }
-
-// var address = '134.249.141.104';
-
-
 
 var address = '0.0.0.0';
 var port = 80;
@@ -20,27 +12,3 @@ app.use(express.static(sitepath))
 app.listen(port, address, function(){
 	console.log('Express server listening on ' + address +':'+ port + " with: " + sitepath)
 });
-
-
-// var connect = require('connect');
-// var serveStatic = require('serve-static');
-
-
-// connect().use(connect.static(__dirname)).listen(port, function(){
-// 		console.log('Express server listening on ' + address +':'+ port);	
-// 	});	
-// connect().use(serveStatic(__dirname)).listen(port, address, function(){
-// 		console.log('Express server listening on ' + address +':'+ port);	
-// 	});	
-
-// var express = require('express');
-// var app = express();
-
-// app.listen(3000, function() {
-//   console.log('Server listening on port 3000');
-// });
-
-
-// var server = http.createServer( app ).listen( PORT, function() {
-//     console.log('Express server listening on port ' + PORT);
-// } );
